0856571c68a8b70b5121fa09b013cd6b1f8b0bfb
FuZzyy14 <simon.delarue2@gmail.com>
2020-09-05 15:48:43 +0200

    Ajout : bouton annuler l'ajout d'une activité

--- app/src/main/java/com/example/sorties2020/vue/VueAjouterActivite.java
+++ app/src/main/java/com/example/sorties2020/vue/VueAjouterActivite.java
@@ -3,6 +3,9 @@ package com.example.sorties2020.vue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.sorties2020.R;
 
@@ -12,5 +15,26 @@ public class VueAjouterActivite extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_ajouter_activite);
+
+        Button vueAjouterActiviteActionAnnuler= (Button)findViewById(R.id.vueAjouterActiviteActionAnnuler);
+        vueAjouterActiviteActionAnnuler.setOnClickListener(
+
+                new View.OnClickListener()
+                {
+                    public void onClick(View arg0) {
+                        // TODO : faire le code du bouton
+
+                        Toast message = Toast.makeText(
+                                getApplicationContext(),
+                                "Action Annuler",
+                                Toast.LENGTH_SHORT);
+                        message.show();
+
+                    }
+                }
+
+
+        );
+
     }
 }
