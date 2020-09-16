2a711a61e3d2e8bcce202e96a7e0c1d7890b799c
FuZzyy14 <simon.delarue2@gmail.com>
2020-09-16 17:47:55 +0200

    Ajout : Premier essaie d'une alarme "fonctionnelle"

--- app/src/main/java/com/example/sorties2020/vue/VueAjouterAlarme.java
+++ app/src/main/java/com/example/sorties2020/vue/VueAjouterAlarme.java
@@ -2,17 +2,27 @@ package com.example.sorties2020.vue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.sorties2020.R;
 
+import java.util.Calendar;
+
 public class VueAjouterAlarme extends AppCompatActivity {
 
-    protected EditText vueAjouterAlarmeChampTemps;
+
+    Context myContext = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,16 +48,33 @@ public class VueAjouterAlarme extends AppCompatActivity {
 
         );
 
-        vueAjouterAlarmeChampTemps = (EditText)findViewById(R.id.vueAjouterAlarmeTemps);
         Button vueAjouterAlarmeActionAjouter= (Button)findViewById(R.id.vueAjouterAlarmeActionAjouter);
         vueAjouterAlarmeActionAjouter.setOnClickListener(
 
                 new View.OnClickListener()
                 {
                     public void onClick(View arg0) {
+                        Calendar dateActuelle = Calendar.getInstance();
+                        final int heure = dateActuelle.get(Calendar.HOUR_OF_DAY);
+                        final int minutes = dateActuelle.get(Calendar.MINUTE);
+                        TimePickerDialog horlogeCadrant = new TimePickerDialog(myContext, new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker timePicker, int heure, int minutes) {
 
-                        enregistrerAlarme();
-                        naviguerRetour();
+                                Intent activerAlarme = new Intent(AlarmClock.ACTION_SET_ALARM);
+                                activerAlarme.putExtra(AlarmClock.EXTRA_HOUR, heure);
+                                activerAlarme.putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+                                if(heure <= 24 && minutes <= 60){
+                                    startActivity(activerAlarme);
+
+                                }
+
+                                naviguerRetour();
+                            }
+                        },heure,minutes,android.text.format.DateFormat.is24HourFormat(myContext));
+
+
+                        horlogeCadrant.show();
                     }
                 }
 
@@ -59,16 +86,5 @@ public class VueAjouterAlarme extends AppCompatActivity {
         this.finish();
     }
 
-    private void enregistrerAlarme(){
-
-
-                        Toast message = Toast.makeText(
-                                getApplicationContext(),
-                                "Ajout d'une alarme",
-                                Toast.LENGTH_SHORT);
-                        message.show();
-
-
-    }
 }
 
